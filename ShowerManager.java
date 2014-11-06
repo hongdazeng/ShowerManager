ce54adf950e2110531e8b24f140c7cb9b7b812b6
Hongda Zeng <hongda.code@gmail.com>
2014-11-05 19:10:43 -0500

    base program v1
    created some helper classes and wrote part of main to create the table
    that will be used to handle reservations

---
+++ ShowerManager.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import javax.swing.*;
+
+public class ShowerManager {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Welcome to Shower Manager 1.0", "Welcome", JOptionPane.PLAIN_MESSAGE);
+
+        int numstall = 0;   // the number of shower stalls availiable in a dorm
+        int starttime = 8;  // start of reserved time in pm
+        int endtime = 8;    // end of reserved time in am
+        int totalhours = 0; // total number of hours managed
+
+        final int maxint = Integer.MAX_VALUE;
+        final int minint = Integer.MIN_VALUE;
+
+        String uin = JOptionPane.showInputDialog(null, "Please enter the number of shower stalls you are managing", "Set number of stalls");
+        numstall = inputProcessor(0, maxint, uin, "Set stall");
+        uin = JOptionPane.showInputDialog(null, "Please enter the start of the managed time \n(in integer pm, midnight is 12 am)", "Set start time");
+        starttime = inputProcessor(0, 13, uin, "Set start time");
+        uin = JOptionPane.showInputDialog(null, "Please enter the end of the managed time \n(in integer am, midnight is 12 am)", "Set start time");
+        endtime = inputProcessor(0, 13, uin, "set end time");
+
+        totalhours = totaltime(starttime, endtime);
+        String[][] timetable = new String[totalhours][numstall];
+
+
+
+    }   // end main
+
+    public static int inputProcessor (int botlimit, int toplimit, String userinput, String reminder) {
+        String uin = userinput;
+        int finalreturn = 0;
+        boolean notgoodenough = true;
+        String erroroutput = ("Please enter a valid number between " + botlimit + " and " + toplimit + "");
+        while (notgoodenough) {
+            try {
+                finalreturn = Integer.parseInt(uin);
+                if (finalreturn > botlimit && finalreturn < toplimit) {
+                    notgoodenough = false;
+                } else {
+                    uin = JOptionPane.showInputDialog(null, erroroutput, reminder);
+                }
+            } catch (Exception e) {
+                uin = JOptionPane.showInputDialog(null, "Please enter a valid integer", reminder);
+            }
+        }
+        return finalreturn;
+    }
+
+    public static int totaltime (int start, int end) {
+        int top = 0;    // total number of hours managed before midnight
+        int bot = 0;    // total number of hours managed after midnight
+
+        if (start == 12) {
+            top = 12;
+        } else {
+            top = 12 - start;
+        }
+
+        if (end == 12) {
+            bot = 1;
+        } else {
+            bot = end + 1;
+        }
+
+        return top + bot;
+    }
+}
